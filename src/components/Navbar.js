64b61d2d61cff69243b0ba99f8501d172e9957a8
Michael Loughman <mcloughman@gmail.com>
2023-04-09 15:01:14 -0700

    Implement useEffect hook in AuthContext to check local storage and update state if there is a user in local storage. In the Navbar, create conditionals to show certain links depending on if user or not. Update css file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,25 +11,26 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <Link to="/">
-        <h1>Hikes</h1>
+      <Link to="/" className="user-nav-div">
+        Hikes
       </Link>
-      <Link to="/login">
-        <h1>Login</h1>
-      </Link>
-      <div>
-        {/* we put this in to show the probelem that we have. when the page is refreshed global state for user is null even though we still have user persisting in local storage. we somehow need to update state to know that user is still actually logged in */}
-        {user && <span>{user.email}</span>}
-        <button onClick={handleClick} className="logout">
-          Logout
-        </button>
-      </div>
-      <Link to="/signup">
-        <h1>Signup</h1>
-      </Link>
-      <Link to="/form">
-        <h1>Add a New Hike</h1>
-      </Link>
+
+      {user && (
+        <div className="user-nav-div">
+          <span>{user.email}</span>
+          <button onClick={handleClick} className="logout">
+            Logout
+          </button>
+          <Link to="/form">Add a new hike</Link>
+        </div>
+      )}
+      {!user && (
+        <div className="user-nav-div">
+          <Link to="/signup">Signup</Link>
+
+          <Link to="/login">Login</Link>
+        </div>
+      )}
     </nav>
   );
 };
